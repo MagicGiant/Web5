0840afaff0bb92afbcaa606b4c499311fbaa4d22
Sherka <obidjanow@mail.ru>
2023-12-23 15:20:10 +0300

    fix and add then-catch

--- js/index.js
+++ js/index.js
@@ -55,75 +55,81 @@ function filterItems(){
     }
 }
 
-async function addImages(albums_count, objects_count, album_id) {
-    try {
-        const objects = await getObjectFromIP("https://jsonplaceholder.typicode.com/photos");
-        const ids = Array.from(getAlbumIds(objects));
+function addImages(albums_count, objects_count, album_id) {
+    let shuffledIds, filteredObjects;
+    getObjectFromIP("https://jsonplaceholder.typicode.com/photos")
+        .then((objects) => {
+            const ids = Array.from(getAlbumIds(objects));
+            shuffledIds = shuffleArray(ids).slice(0, albums_count);
+            return filterObjectsByIds(objects, shuffledIds);
+        })
+        .then((result) => {
+            filteredObjects = result;
+            const container = document.getElementById("image");
+            return Promise.all(
+                shuffledIds.map(async (id) => {
+                    if (album_id !== -1 && album_id !== id) {
+                        return;
+                    }
 
-        const shuffledIds = shuffleArray(ids).slice(0, albums_count);
-        const filteredObjects = await filterObjectsByIds(objects, shuffledIds);
+                    const albumConteiner = document.createElement("div");
+                    const album = document.createElement("div");
+                    const gradLine = document.createElement("hr");
+                    const titleText = document.createElement("span");
 
-        conteiner = document.getElementById("image");
-        for (const id of shuffledIds) {
-            if (album_id != -1 && album_id != id){
-                continue;
-            }
+                    albumConteiner.id = "album" + id;
+                    albumConteiner.className = "album_conteiner";
 
-            var albumConteiner = document.createElement("div");
-            var album = document.createElement("div");
-            var gradLine = document.createElement("hr");
-            var titleText = document.createElement("span");
+                    album.className = "main__album";
+                    gradLine.className = "grad_line";
+                    titleText.className = "main__album-name";
+                    titleText.textContent = "Альбом: " + id;
 
-            albumConteiner.id = "album" + id
-            albumConteiner.className = "album_conteiner"
+                    container.appendChild(albumConteiner);
+                    albumConteiner.appendChild(album);
+                    album.appendChild(gradLine);
+                    album.appendChild(titleText);
+                    album.appendChild(gradLine.cloneNode(true));
 
-            album.className = "main__album";
-            gradLine.className = "grad_line";
-            titleText.className = "main__album-name";
-            titleText.textContent = "Альбом: " + id
+                    const examples = document.createElement("div");
+                    examples.className = "main__examples";
+                    albumConteiner.appendChild(examples);
 
-            conteiner.appendChild(albumConteiner);
-            albumConteiner.appendChild(album);
-            album.appendChild(gradLine);
-            album.appendChild(titleText);
-            album.appendChild(gradLine.cloneNode(true));
+                    let objects_count_current = 0;
+                    for (const object of result) {
+                        if (object.albumId !== id) {
+                            continue;
+                        }
+                        if (objects_count_current === objects_count) {
+                            break;
+                        }
 
-            var examples = document.createElement("div");
-            examples.className = "main__examples";
-            albumConteiner.appendChild(examples);
+                        objects_count_current++;
 
-            let objects_count_current = 0
-            for (const object of filteredObjects) {
-                if (object.albumId != id){
-                    continue;
-                }
-                if (objects_count_current === objects_count){
-                    break;
-                }
-                
-                objects_count_current++;
+                        const example = document.createElement("div");
+                        const img = document.createElement("img");
 
-                var example = document.createElement("div")
-                var img = document.createElement("img")
+                        example.className = "main__examples-example";
+                        img.className = "main__examples-example-img";
 
-                example.className = "main__examples-example"
-                img.className = "main__examples-example-img"
+                        img.alt = "example";
+                        img.src = object.url;
 
-                img.alt = "example"
-                img.src = object.url
+                        examples.appendChild(example);
+                        example.appendChild(img);
+                    }
 
-                examples.appendChild(example)
-                example.appendChild(img)
-            }
-
-            await filterObjectsByIds(objects, shuffledIds);
-        }
-
-        console.log(shuffledIds);
-        console.log(filteredObjects)
-    } catch (error) {
-        console.error(error);
-    }
+                    // await filterObjectsByIds(objects, shuffledIds);
+                })
+            );
+        })
+        .then(() => {
+            console.log(shuffledIds);
+            console.log(filteredObjects);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
 }
 
 function getAlbumIds(objects) {
